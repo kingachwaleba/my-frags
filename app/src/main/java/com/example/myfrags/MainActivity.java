eafc8f761e9f15ab3f719f4f7615ef876a7e335d
kingachwaleba <kingach6@gmail.com>
2021-10-19 10:59:01 +0200

    Edit MainActivity

--- app/src/main/java/com/example/myfrags/MainActivity.java
+++ app/src/main/java/com/example/myfrags/MainActivity.java
@@ -1,35 +1,80 @@
 package com.example.myfrags;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity implements Fragment1.OnButtonClickListener {
 
-    private FragmentManager fragmentManager;
-    private Fragment fragment1, fragment2, fragment3, fragment4;
+    private int[] frames;
+    private boolean hidden;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fragment1 = new Fragment1();
-        fragment2 = new Fragment2();
-        fragment3 = new Fragment3();
-        fragment4 = new Fragment4();
+        if (savedInstanceState == null) {
+            frames = new int[]{R.id.frame1, R.id.frame2, R.id.frame3, R.id.frame4};
+            hidden = false;
 
-        fragmentManager = getSupportFragmentManager();
+            Fragment[] fragments = new Fragment[]{new Fragment1(), new Fragment2(), new Fragment3(),
+                    new Fragment4()};
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
 
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.add(R.id.frame1, fragment1);
-        transaction.add(R.id.frame2, fragment2);
-        transaction.add(R.id.frame3, fragment3);
-        transaction.add(R.id.frame4, fragment4);
-        transaction.addToBackStack(null);
-        transaction.commit();
+            for (int i = 0; i < 4; i ++) {
+                transaction.add(frames[i], fragments[i]);
+            }
+
+            transaction.addToBackStack(null);
+            transaction.commit();
+        }
+        else {
+            frames = savedInstanceState.getIntArray("FRAMES");
+            hidden = savedInstanceState.getBoolean("HIDDEN");
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putIntArray("FRAMES", frames);
+        outState.putBoolean("HIDDEN", hidden);
+    }
+
+    @Override
+    public void onButtonClickShuffle() {
+        Toast.makeText(getApplicationContext(), "Shuffle", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onButtonClickClockwise() {
+        Toast.makeText(getApplicationContext(), "Clockwise", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onButtonClickHide() {
+        Toast.makeText(getApplicationContext(), "Hide", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onButtonClickRestore() {
+        Toast.makeText(getApplicationContext(), "Restore", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onAttachFragment(@NonNull Fragment fragment) {
+        super.onAttachFragment(fragment);
+
+        if (fragment instanceof  Fragment1) {
+            ((Fragment1) fragment).setOnButtonClickListener(this);
+        }
     }
 }
